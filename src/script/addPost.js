45afcd57f589c5e7baae432f3303b48b40132fc6
Gabriel Augusto <gabrielaugusto.lv@hotmail.com>
2025-06-03 22:33:28 -0300

    Alterações minoritárias em addPost e editor.html

--- src/script/addPost.js
+++ src/script/addPost.js
@@ -2,15 +2,16 @@
 
 db.collection("post").add({
   titulo: "Como economizar energia em casa",
+  subtitulo: "Dicas práticas para reduzir o consumo de energia elétrica",
   conteudo: "Desligue aparelhos da tomada quando não estiverem em uso...",
   categoria: "Energia",
   dataPublicacao: new Date(),
   imagemURL: "https://firebase.storage.url/eco-dicas/energia.jpg",
-  status: "ativo"
+  status: true
 })
 .then((docRef) => {
   console.log("Post criado com ID:", docRef.id);
 })
 .catch((error) => {
-  console.error("Erro ao criar post:", error);
+  console.error("Erro ao criar post:", error);
 });
